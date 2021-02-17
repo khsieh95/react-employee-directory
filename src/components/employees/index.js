368c97ed0b41ba03a97a007bc3f8096677c5bf75
Kevin Hsieh <khsieh95@gmail.com>
2021-02-16 16:55:27 -0800

    table renders information and random ppl

--- src/components/employees/index.js
+++ src/components/employees/index.js
@@ -3,6 +3,9 @@ import React from "react";
 function showEmployee(props) {
   return (
     <tr>
+      <th scope="row">
+        <img src={props.photo} />
+      </th>
       <td>{props.firstName}</td>
       <td>{props.lastName}</td>
       <td>{props.city}</td>
--- src/components/table/index.js
+++ src/components/table/index.js
@@ -20,6 +20,20 @@ class table extends Component {
       });
     });
   }
+  // Input Change
+  handleInputChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+    console.log(this.state.firstName);
+  };
+
+  searchByName = () => {
+    const filtered = this.state.employees.filter(
+      (obj) => obj.name.first === this.state.firstName
+    );
+    this.setState({ employees: filtered });
+  };
+
   // Sort top to bottom vs bottom to top
   handleFirstName = () => {
     const sort = this.state.sortEmployees.sort((k, h) => {
@@ -55,6 +69,50 @@ class table extends Component {
             Search
           </button>
         </form>
+        <table className="table table-primary table-striped">
+          <thead className="thead">
+            <tr className="tablehead">
+              <th scope="col">Photo</th>
+              <th
+                scope="col"
+                onClick={this.handleFirstName}
+                className="firstName"
+              >
+                First Name
+              </th>
+              <th
+                scope="col"
+                onClick={this.handleLastName}
+                className="lastName"
+              >
+                Last Name
+              </th>
+              <th
+                scope="col"
+                onClick={this.handleLocation}
+                className="locationState"
+              >
+                Location
+              </th>
+              <th scope="col">Email</th>
+              <th scope="col">Phone</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.employees.map((employee) => (
+              <Employees
+                key={employee.login.uuid}
+                photo={employee.picture.thumbnail}
+                firstName={employee.name.first}
+                lastName={employee.name.last}
+                city={employee.location.city}
+                state={employee.location.state}
+                email={employee.email}
+                phone={employee.cell}
+              />
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
